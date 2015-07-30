d69eacc865a2ab6909817ad2eb3a21b42fa788ad
jsimonelli <jose.simonelli@gmail.com>
2015-07-30 15:20:21 -0400

    first commit

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+PROJECTS=( "miq-F5" "miq-BlueCat" "miq-InfoBlox" "miq-Amazon" )
+
+install_rake_helper() {
+cd /tmp
+
+if [ -d cfme-rhconsulting-scripts-master ] ; then
+    rm -fR /tmp/cfme-rhconsulting-scripts-master
+fi
+
+wget -O cfme-rhconsulting-scripts.zip https://github.com/rhtconsulting/cfme-rhconsulting-scripts/archive/master.zip
+unzip cfme-rhconsulting-scripts.zip
+cd cfme-rhconsulting-scripts-master
+make install
+}
+
+load_projects() {
+PROJECT_NAME="${1}"
+PROJECT_ZIP="https://github.com/rhtconsulting/${1}/archive/master.zip"
+cd /tmp
+wget -O ${PROJECT_NAME}.zip ${PROJECT_ZIP}
+unzip ${PROJECT_NAME}.zip
+cd ${PROJECT_NAME}-master
+sh install.sh
+}
+
+
+
+install_rake_helper
+for NAME in ${PROJECTS[@]} ; do
+    load_projects ${NAME}
+done
